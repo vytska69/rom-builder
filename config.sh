0dd4b79b0a71abe8d2952c4e663143e18873e671
vytska69 <79152715+vytska69@users.noreply.github.com>
2021-02-16 16:41:36 +0200

    added build options of pixel experience for pine

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=vytska69
+GITHUB_EMAIL=vytautas.duchas@gmail.com
 
 KBUILD_BUILD_USER="Aki"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-export oem=motorola
-export device=harpia
+export oem=xiaomi
+export device=pine
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+branch="q"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="vytska69/pine-roms"
 
-timezone="Asia/Kolkata"
+timezone="Europia/Vilnius"
